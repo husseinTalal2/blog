9849276fcd841cc46dcc7ecc0155824904dae7e3
husseinTalal2 <hossientalal2@gmail.com>
2023-05-12 21:56:03 +0300

    init commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed
+            return redirect('/dashboard');
+        }
+
+        // Authentication failed
+        return back()->withErrors(['email' => 'Invalid credentials']);
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+}
